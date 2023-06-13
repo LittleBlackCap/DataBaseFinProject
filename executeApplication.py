bdf83415e28d7308946a01b77527a1006cad3761
LittleBlackCap <119925944+LittleBlackCap@users.noreply.github.com>
2023-06-14 03:30:43 +0800

    Add files via upload

--- executeApplication.py
+++ executeApplication.py
@@ -1,39 +1,43 @@
 # coding=utf-8
 import mariadb
-import sqlite3
+
 
 def create_connection():
     try:
-        user_setting={
-            "user":"411077026",
-            "password":"411077026",
-            "host":"140.127.74.226",
-            "database":"411077026"}
+        user_setting = {
+            "user": "411077014",
+            "password": "411077014",
+            "host": "140.127.74.226",
+            "database": "411077014",
+        }
         connection = mariadb.connect(**user_setting)
         return connection
     except mariadb.Error as e:
         print(f"資料庫錯誤: {e}\n")
         return None
 
+
 def close_connection(connection):
     connection.close()
-    
+
+
 def command():
     # if(position==1):
-        # print("search something where 查詢本地或附近的商品庫存\n")
+    # print("search something where 查詢本地或附近的商品庫存\n")
     # elif(position==2):
-        # print("insert phonenumber 訪問顧客數據、輸入電話訂單\n")
+    # print("insert phonenumber 訪問顧客數據、輸入電話訂單\n")
     # elif(position==3):
-        # print("record something where 記錄進貨和更新庫存\n")
+    # print("record something where 記錄進貨和更新庫存\n")
     print("maxCostCustomer 查找過去一年中累積金額最高的顧客")
-    print("searchTopTwoTotalPrice 查找過去一年中按銷售金額計算的前2個產品")
+    print("topTwoTotalPrice 查找過去一年中按銷售金額計算的前2個產品")
     print("notInKaohsiung 查找所有在高雄的商店都缺貨的產品")
     print("findDelay 查找未按照承諾的時間交付的包裹")
-    print("findruined 查找在事故中損壞包裹的顧客聯繫信息。此外，查找該包裹的內容並創建替換商品的新包裹。")
-    print("search something where 查詢本地或附近的商品庫存")
-    print("insert phonenumber 訪問顧客數據、輸入電話訂單")
-    print("record something where 記錄進貨和更新庫存")
+    print("findruined 查找在事故中損壞包裹的顧客聯繫信息。此外，查找該包裹的內容並創建替換商品的新包裹。")  # 還有問題
+    print("search something(item_id) where(depot,Taipei,Kaohsiung) 查詢本地或附近的商品庫存")
+    print("insert phonenumber 訪問顧客數據、輸入電話訂單")  # 還有問題
+    print("record something where 記錄進貨和更新庫存")  # 還有問題
     print()
+
 
 def maxCostCustomer(connection):
     try:
@@ -51,26 +55,32 @@ def maxCostCustomer(connection):
         cursor.close()
     except mariadb.Error as e:
         print(f"資料庫錯誤: {e}\n")
-        
+
+
 def topTwoTotalPrice(connection):
     try:
         cursor = connection.cursor()
-        query ="""
+        query = """
             SELECT item1_number, item2_number, item3_number, item4_number, item5_number, item6_number, item7_number, item8_number, item9_number, item10_number, item11_number, item12_number, item13_number, item14_number, item15_number, item16_number, item17_number, item18_number, item19_number, item20_number, item21_number, item22_number, item23_number, item24_number 
             FROM sale_data 
-            WHERE when='total_price'
+            WHERE whatTime='total_price'
         """
         cursor.execute(query)
         row = cursor.fetchall()
-        data = list(row)
-        largest_value = max(data)
-        #不對啊，這樣還是不知道是哪個商品。而且還只有最大的
-        query ="""
-            SELECT item1_number, item2_number, item3_number, item4_number, item5_number, item6_number, item7_number, item8_number, item9_number, item10_number, item11_number, item12_number, item13_number, item14_number, item15_number, item16_number, item17_number, item18_number, item19_number, item20_number, item21_number, item22_number, item23_number, item24_number 
-            FROM sale_data 
-            WHERE largest_value='total_price'
+        data = list(row[0])
+        maximum = max(data)
+        maxIndex = data.index(maximum)
+        data2 = data
+        data2.remove(maximum)
+        second = max(data2)
+        secondIndex = data.index(second)
+
+        query = """
+            SELECT item_name
+            FROM item 
+            WHERE (item_id=(%s) OR item_id=(%s))
         """
-        cursor.execute(query)
+        cursor.execute(query, (maxIndex + 1, secondIndex + 1))
         result = cursor.fetchall()
         for row in result:
             print(row)
@@ -78,7 +88,8 @@ def topTwoTotalPrice(connection):
         cursor.close()
     except mariadb.Error as e:
         print(f"資料庫錯誤: {e}\n")
-        
+
+
 def notInKaohsiung(connection):
     try:
         cursor = connection.cursor()
@@ -96,11 +107,16 @@ def notInKaohsiung(connection):
         cursor.close()
     except mariadb.Error as e:
         print(f"資料庫錯誤: {e}\n")
-        
+
+
 def findDelay(connection):
     try:
         cursor = connection.cursor()
-        query = f"SELECT order_id FROM trans_info WHERE delay=1"
+        query = """
+            SELECT order_id 
+            FROM trans_info 
+            WHERE delay=1
+        """
         cursor.execute(query)
         result = cursor.fetchall()
         for row in result:
@@ -109,33 +125,107 @@ def findDelay(connection):
         cursor.close()
     except mariadb.Error as e:
         print(f"資料庫錯誤: {e}\n")
-        
+
+
 def findruined(connection):
     try:
         cursor = connection.cursor()
-        cus1 = f"select customer from trans_info,order,customer where (trans_info.state=”損壞” AND trans_info.order_id= order.order_id AND order.cus_id = customer.cus_id)"
-        query1 = f"select customer.phone_num from trans_info,order,customer where (trans_info.state=”損壞” AND trans_info.order_id= order.order_id AND order.cus_id = customer.cus_id)"
-        cursor.execute(query1)
+        query = """
+            SELECT DISTINCT customer.cus_id 
+            FROM trans_info, customer 
+            WHERE (trans_info.state = '物件毀損' AND trans_info.cus_id = customer.cus_id)
+        """
+        cursor.execute(query)
+        cus1 = cursor.fetchall()
+        print("costumer: ",end="")
+        for row in cus1:
+            print(row)
+
+        query = """
+            SELECT phone_num 
+            FROM customer 
+            WHERE cus_id= (%s)
+        """
+        cursor.execute(query, (cus1,))
         result = cursor.fetchall()
+        print("此名顧客的聯絡方式為: ", end="")
         for row in result:
             print(row)
-        id1 = f"SELECT order_id FROM order ORDER BY id DESC LIMIT 0 , 1"
-        query2 = f"INSERT INTO order VALUES (id1[0]+(int(id[1:3])+1),(SELECT order.cus_id,order.price FROM order,trans_info WHERE (trans_info.state=”損壞” AND trans_info.order_id= order.order_id)),CURDATE())"
-        cursor.execute(query2)
+
+        # query = """
+        #     SELECT SUM(item.item_price)
+        #     FROM item,product,trans_info
+        #     WHERE (trans_info.state='物件毀損' AND trans_info.order_id = product.order AND product.item_id = item.item_id)
+        # """
+        # query = """
+        #     SELECT SUM(item_price)
+        #     FROM item
+        #     WHERE item.id IN %s
+        # """
+        # cursor.execute(query, (items))
+        # items_sum = cursor.fetchall()
+
+
+        query = """
+            SELECT item_id 
+            FROM product 
+            WHERE state = '物件毀損'
+        """
+        cursor.execute(query)
+        items = cursor.fetchall()
+        for row in items:
+            print(row)
+
+        query = """
+            SELECT SUM(item_price)
+            FROM item
+            WHERE item_id IN %s
+        """
+        cursor.execute(query, (items,))
+        items_sum = cursor.fetchall()
+        for row in items_sum:
+            print(row)
+
+        query = """
+            SELECT order_id
+            FROM orders
+            ORDER BY order_id DESC
+        """
+        cursor.execute(query, (items,))
+        lastOrder_id = cursor.fetchone()
+        for row in lastOrder_id:
+            print(row)
+
+        query = "INSERT INTO orders VALUES ((%s[0]+(int(%s)[1:len(%s)])+1),%s,%s,CURDATE())"
+        cursor.execute(query, (lastOrder_id, lastOrder_id, lastOrder_id, cus1, items_sum))
         connection.commit()
         print()
         cursor.close()
     except mariadb.Error as e:
         print(f"資料庫錯誤: {e}\n")
 
+
 def searchInventory(connection, sth, whe):
     try:
         cursor = connection.cursor()
-        query = f"SELECT amount FROM {whe} WHERE name=={sth}"
-        cursor.execute(query)
-        result = cursor.fetchall()
-        for row in result:
-            print(row)
+        if whe == "depot":
+            query = f"SELECT store_num FROM depot WHERE item_id={sth}"
+            cursor.execute(query)
+            result = cursor.fetchall()
+            for row in result:
+                print(row)
+        elif whe == "Taipei":
+            query = f"SELECT Taipei_store FROM shop WHERE item_id={sth}"
+            cursor.execute(query)
+            result = cursor.fetchall()
+            for row in result:
+                print(row)
+        else:
+            query = f"SELECT Kaohsiung_store FROM shop WHERE item_id={sth}"
+            cursor.execute(query)
+            result = cursor.fetchall()
+            for row in result:
+                print(row)
         print()
         cursor.close()
     except mariadb.Error as e:
@@ -146,8 +236,10 @@ def insertOrder(connection, phoneNumber):
     try:
         cursor = connection.cursor()
         # insert 55
-        # query = f"INSERT INTO order VALUES (SELECT name FROM customer WHERE phone={phoneNumber})"
-        query = f"INSERT INTO test VALUES ((SELECT id FROM test2 WHERE habbit='我愛音樂'),'')"
+        # query = f"INSERT INTO orders VALUES (B131,(SELECT cus_id FROM customer WHERE phone={phoneNumber}),)"
+        query = (
+            f"INSERT INTO orders VALUES ((SELECT id FROM test2 WHERE habbit='我愛音樂'),'')"
+        )
         cursor.execute(query)
         connection.commit()
         print()
@@ -155,11 +247,14 @@ def insertOrder(connection, phoneNumber):
     except mariadb.Error as e:
         print(f"資料庫錯誤: {e}\n")
 
+
 def recordInventory(connection, sth, where):
     try:
         cursor = connection.cursor()
         # record id test2
-        query = f"INSERT INTO {where} VALUES (555, (SELECT id FROM test WHERE name='df'))"
+        query = (
+            f"INSERT INTO {where} VALUES (555, (SELECT id FROM test WHERE name='df'))"
+        )
         # query = f"INSERT INTO {where} VALUES ('SELECT amount FROM {where} WHERE {sth}==name',{where})"
         cursor.execute(query)
         connection.commit()
@@ -167,7 +262,7 @@ def recordInventory(connection, sth, where):
         cursor.close()
     except mariadb.Error as e:
         print(f"資料庫錯誤: {e}\n")
-    
+
 
 def main():
     print("請稍候......")
@@ -175,32 +270,33 @@ def main():
     # print("請輸入您的身份(數字): 1 Customer service/ 2 Call center staff/ 3 The stocking clerks")
     # position=int(input())
     print("(若不知要查詢什麼可以按「？」、若要結束按「bye」)")
-    while(1):
-        print("要查詢:",end=" ")
-        str=list(input().split())
-        if(str[0]=='?'):
+    while 1:
+        print("要查詢:", end=" ")
+        str = list(input().split())
+        if str[0] == "?":
             command()
-        # elif(str[0]=='search'):
-            # searchInventory(connection, str[1], str[2])
-        # elif(str[0]=='insert'):
-            # insertOrder(connection, str[1])
-        # elif(str[0]=='record'):
-            # recordInventory(connection, str[1], str[2])
-        elif(str[0]=='maxCostCustomer'):
+        elif str[0] == "search":
+            searchInventory(connection, str[1], str[2])
+        elif str[0] == "insert":
+            insertOrder(connection, str[1])
+        elif str[0] == "record":
+            recordInventory(connection, str[1], str[2])
+        elif str[0] == "maxCostCustomer":
             maxCostCustomer(connection)
-        elif(str[0]=='topTwoTotalPrice'):
+        elif str[0] == "topTwoTotalPrice":
             topTwoTotalPrice(connection)
-        elif(str[0]=='notInKaohsiung'):
+        elif str[0] == "notInKaohsiung":
             notInKaohsiung(connection)
-        elif(str[0]=='findDelay'):
+        elif str[0] == "findDelay":
             findDelay(connection)
-        elif(str[0]=='findruined'):
+        elif str[0] == "findruined":
             findruined(connection)
-        elif(str[0]=='bye'):
-            break;
+        elif str[0] == "bye":
+            break
         else:
             print("輸錯指令了請重來")
     close_connection(connection)
+
 
 if __name__ == "__main__":
     main()
